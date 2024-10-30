691a6721eeaa2d508efd8f05d28c15987d062170
K-Othman <karim_othman88@hotmail.com>
2024-10-30 22:32:29 +0000

    building the background image for the landing page

--- src/app/components/LandingPage.tsx
+++ src/app/components/LandingPage.tsx
@@ -1,12 +1,16 @@
 const LandingPage = () => {
   return (
-    <section className="container bg-neutral-500 h-screen mx-auto ">
+    <section className="h-screen max-w-screen mx-auto">
       <div
-        className="bg-cover bg-center h-screen"
+        className="bg-cover bg-center h-screen -mt-9"
         style={{ backgroundImage: "url('/images/home_background.jpg')" }}
-      ></div>
-      <div className="mx-auto">
-        <h1>Welcome To My Bookshop</h1>
+      >
+        <span className="absolute h-full w-full bg-black bg-opacity-50"></span>
+      </div>
+      <div className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]">
+        <h1 className="text-[#FCFCEA]">
+          Welcome To <span>My Bookshop</span>{" "}
+        </h1>
         <input type="search" placeholder="Search Books" />
       </div>
     </section>
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="bg-blue-500 text-white p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <Link href="/">Bookshop</Link>
+    <nav className="p-4">
+      <div className="container z-10 text-[#FCFCEA] mx-auto flex justify-between items-center absolute">
+        <Link href="/">My Bookshop</Link>
         <div className="space-x-4">
           <Link href="/books">Books</Link>
           <Link href="/cart">Cart</Link>
