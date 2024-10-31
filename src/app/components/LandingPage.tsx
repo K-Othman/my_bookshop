53a85e56a77e2be594a91ae3fb771d7690d885e9
K-Othman <karim_othman88@hotmail.com>
2024-10-31 15:50:18 +0000

    creating the headings

--- src/app/components/LandingPage.tsx
+++ src/app/components/LandingPage.tsx
@@ -2,16 +2,23 @@ const LandingPage = () => {
   return (
     <section className="h-screen max-w-screen mx-auto">
       <div
-        className="bg-cover bg-center h-screen -mt-9"
+        className="bg-cover bg-center h-[70vh] -mt-9"
         style={{ backgroundImage: "url('/images/home_background.jpg')" }}
       >
         <span className="absolute h-full w-full bg-black bg-opacity-50"></span>
       </div>
-      <div className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]">
-        <h1 className="text-[#FCFCEA]">
-          Welcome To <span>My Bookshop</span>{" "}
-        </h1>
-        <input type="search" placeholder="Search Books" />
+      <div className="absolute top-[30%] left-[50%] -translate-x-[50%] -translate-y-[50%] ">
+        <div className="text-[#FCFCEA] text-center w-full">
+          <div className="text-5xl">Welcome to</div>
+          <h1 className="text-8xl lette tracking-wider">My Bookshop</h1>
+        </div>
+        <div className="mt-5">
+          <input
+            className="w-full rounded-3xl py-4 px-2"
+            type="search"
+            placeholder="Search Books"
+          />
+        </div>
       </div>
     </section>
   );
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Navbar: React.FC = () => {
   return (
     <nav className="p-4">
-      <div className="container z-10 text-[#FCFCEA] mx-auto flex justify-between items-center absolute">
+      <div className="container z-10 text-[#FCFCEA] flex justify-between items-center absolute">
         <Link href="/">My Bookshop</Link>
         <div className="space-x-4">
           <Link href="/books">Books</Link>
