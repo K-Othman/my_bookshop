1d4503e61c3337215ad512949cf3fe0517563c39
K-Othman <karim_othman88@hotmail.com>
2024-11-05 23:58:21 +0000

    fixed the margin issue in mobile screen and added hover colors to the navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,12 +2,33 @@ import Link from "next/link";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className=" container z-10 relative">
-      <div className="mx-2 text-[#FCFCEA] flex justify-between items-center">
-        <Link href="/">My Bookshop</Link>
-        <div className="space-x-4">
-          <Link href="/books">Books</Link>
-          <Link href="/cart">Cart</Link>
+    <nav className="z-10 relative">
+      <div className="px-3 md:px-0 container md:mx-auto text-[#FCFCEA] flex justify-between items-center">
+        <Link
+          href="/"
+          className="text-[#FCFCEA] hover:text-gray-300 transition-all font-bold"
+        >
+          My Bookshop
+          {/* <Image
+            src={"/images/logo.webp"}
+            alt="My Bookshop Logo"
+            width={50}
+            height={50}
+          /> */}
+        </Link>
+        <div className="space-x-4 ">
+          <Link
+            className="text-[#FCFCEA] hover:text-gray-300 transition-all"
+            href="/books"
+          >
+            Books
+          </Link>
+          <Link
+            className="text-[#FCFCEA] hover:text-gray-300 transition-all"
+            href="/cart"
+          >
+            Cart
+          </Link>
         </div>
       </div>
     </nav>
