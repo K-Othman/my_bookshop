92e9ed42a2e4b0ab0a40b9c0d18ebe87a1edfe1d
K-Othman <karim_othman88@hotmail.com>
2024-11-13 22:33:46 +0000

    styling the books

--- src/app/components/Books.tsx
+++ src/app/components/Books.tsx
@@ -33,15 +33,19 @@ const Books = () => {
       });
   }, []);
 
+  //   const filterByPrice = (books) =>{
+
+  //   }
+
   if (isLoading) return <p>Loading ...</p>;
   if (!books.length) return <p>No Books</p>;
 
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 text-center">
+    <div className="grid grid-cols-2 gap-8 md:grid-cols-3 text-center">
       {books.map((book) => (
         <div key={book.isbn13} className="">
-          <Link href={book.url} className="">
-            <img src={book.image} alt="The Book Cover" />
+          <Link href={book.url} className="text-center">
+            <img className="mx-auto" src={book.image} alt="The Book Cover" />
             <div className="-mt-5">
               <h2 className="font-bold">{book.title}</h2>
               <p>{book.price}</p>
